4f131d009a94d0dac066300af9c6dc92e913f27d
Katya Varfolomeeva <eavarfolomeeva@rt-dc.ru>
2020-06-25 17:31:40 +0300

    Change structures for reading configuration files

--- config/config.go
+++ config/config.go
@@ -4,46 +4,60 @@ import(
 	"io/ioutil"
 	"gopkg.in/yaml.v2"
 	"fmt"
+	"github.com/sirupsen/logrus"
 )
 
-type TGeneralConfig struct {
-	Graphite TGraphiteConfig `yaml:"graphite"`
-	Api TApiTuningManagerConfig `yaml:"tm_api"`
-	Loggers []TLoggingConfig `yaml:"logging"`
+//Типы данных для основного конфигурационного файла
+type TGeneral struct {
+	Graphite TGraphite `yaml:"graphite"`
+	Api TApiTuningManager `yaml:"tm_api"`
+	Storages []TStorage `yaml:"storages"`
+	Loggers []TLogging `yaml:"logging"`
 }
 
-type TGraphiteConfig struct {
+type TGraphite struct {
 	Host string `yaml:"host"`
 	Port int `yaml:"port"`
 	Interval int `yaml:"interval"`
 }
 
-type TApiTuningManagerConfig struct {
+type TApiTuningManager struct {
 	Host string `yaml:"host"`
-	Port int `yaml:"port"`
+	Port string `yaml:"port"`
 	Protocol string `yaml:"proto"`
 	User string `yaml:"user"`
 	Password string `yaml:"password"`
 }
 
-type TStorageConfig struct {
-	Storages []TStorage `yaml:"storages"`
-}
-
 type TStorage struct {
-	Serial_Num string `yaml:"serial_number"`
+	Serial_Num string `yaml:"serialNumber"`
 	Type string `yaml:"type"`
-	Name string `yaml:"name"`
+	Name string `yaml:"visibleName"`
+	HostName string `yaml:"hostName"`
+	InstanceName string `yaml:"instanceName"`
 }
 
-type TLoggingConfig struct {
+type TLogging struct {
 	LoggerName string `yaml:"logger"`
 	File string `yaml:"file"`
 	Level string `yaml:"level"`
 	Encoding string `yaml:"encoding"`
 }
 
-var GeneralConfig = TGeneralConfig{}
+//Типы данных для файла с метриками
+type TResourceGroups struct {
+	Perf []TResource `yaml:"perf"`
+	Capacity []TResource `yaml:"capacity"`
+}
+
+type TResource struct {
+	Name string `yaml:"name"`
+	Label string `yaml:"label"`
+	Target string `yaml:"target"`
+	Type string `yaml:"type"`
+}
+
+var General = TGeneral{}
 
 func GetConfig(configPath string) (err error){
 	var buff []byte
@@ -52,10 +66,41 @@ func GetConfig(configPath string) (err error){
 		fmt.Println("Failed to read config", err)
 		return
 	}
-	err = yaml.Unmarshal(buff, &GeneralConfig)
+	err = yaml.Unmarshal(buff, &General)
 	if err!=nil{
 		fmt.Println("Failed to decode document", err)
 		return
 	}
 	return nil
 }
+
+var ResourceGroups = TResourceGroups{}
+
+func GetResourceConfig(log *logrus.Logger, path string)(err error){
+	var buff []byte
+	buff, err = ioutil.ReadFile(path)
+	if err!=nil{
+		log.Warning("Failed to read config: Error: ", err)
+	}
+	err = yaml.Unmarshal(buff, &ResourceGroups)
+	if err!=nil{
+		log.Warning("Failed to decode document: Error: ", err)
+		return
+	}
+	return nil
+}
+
+/*func GetResourceConfig(log *logrus.Logger, path string)(ResourceConfig TResourceConfig, err error){
+	var buff []byte
+	buff, err = ioutil.ReadFile(path)
+	if err!=nil{
+		log.Warning("Failed to read config: Error: ", err)
+		return
+	}
+	err = yaml.Unmarshal(buff, &ResourceConfig)
+	if err!=nil{
+		log.Warning("Failed to decode document: Error: ", err)
+		return
+	}
+	return
+}*/
